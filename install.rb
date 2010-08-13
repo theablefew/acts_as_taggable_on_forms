d5048ca6c3217309785bb13aa096dbe2566e85ed
Josh Aronson <josh.aronson@skyroo.com>
2010-08-13 09:43:23 -0500

    update install hook

--- install.rb
+++ install.rb
@@ -4,13 +4,13 @@ require File.dirname(__FILE__) + "/../../../config/environment"
 
 this    = File.expand_path(File.dirname(__FILE__))
 target  = "acts_as_taggable_on_forms.js" 
-destination  = File.join(RAILS_ROOT, "public", "javascripts")
+dest    = File.join(RAILS_ROOT, "public", "javascripts")
 
-if File.exists?("#{RAILS_ROOT}/public/javascripts/#{target}")
-  print "#{target} exists in #{destination}. Overwrite?"
-  if gets.chomp.strip.downcase == "y"
-    File.copy(File.join(this, "lib", "acts_as_taggable_on_forms", target), destination)
-  end
-else
-  File.copy(File.join(this, "lib", "acts_as_taggable_on_forms", target), destination)
-end
+puts "Copying #{target} to #{dest}"
+#if File.exists?("#{RAILS_ROOT}/public/javascripts/#{target}")
+  File.copy(File.join(this, "lib", "acts_as_taggable_on_forms", target), dest)
+#end
+
+puts %Q?
+Be sure to include #{target} in the your views when using acts_as_taggable_on_forms!
+?
