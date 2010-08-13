d9f9a6a46ea84739df64d79e113756559a9f05b1
Michael Bishop <mike@theablefew.com>
2010-08-13 16:08:53 -0500

    reordered tag list

--- lib/acts_as_taggable_on_forms/form_extensions.rb
+++ lib/acts_as_taggable_on_forms/form_extensions.rb
@@ -1,8 +1,8 @@
 module ActsAsTaggableOnForms
   module FormExtensions
     def tag_field(field_name, tag_name)
-      existing_tags    = self.object.class.section_counts
-      additional_tags  = self.object.section_list
+      existing_tags    = self.object.section_list
+      additional_tags  = self.object.class.section_counts
       @template.render(
         :file => "#{RAILS_ROOT}/vendor/plugins/acts_as_taggable_on_forms/lib/acts_as_taggable_on_forms/_tag_field.html.erb",
         :locals => {
